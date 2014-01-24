a5c326c9f4c3cbcf4ab579b39badce813d868eff
pikesley <github@orgraphone.org>
2014-01-24 09:41:52 +0000

    5m because WHY NOT?

--- jobs/glucose.rb
+++ jobs/glucose.rb
@@ -1,4 +1,4 @@
-SCHEDULER.every '1h', :first_at => Time.now do
+SCHEDULER.every '5m', :first_at => Time.now do
   points = DiabetesDashboard.glucose(7)
 
   send_event('blood-glucose', points: points)
